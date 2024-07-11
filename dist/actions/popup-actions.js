82cf34a71767d1d79b39b33af0fcf2d85ea0e682
Batman4496 <ys861438@gmail.com>
2024-07-11 20:44:06 +0530

    stuff

--- dist/actions/popup-actions.js
+++
@@ -1,6 +0,0 @@
-export function popupSubmit(e, audioEmbed, url) {
-    const { quill } = audioEmbed;
-    const index = quill.getSelection();
-    quill.insertEmbed(index, 'ql-audio-blot', url);
-    audioEmbed.popupToggle(audioEmbed.popup);
-}
--- dist/helpers.js
+++
@@ -1,6 +0,0 @@
-export function setStyles(element, styles) {
-    Object.keys(styles).map((key) => {
-        element.style[key] = styles[key];
-    });
-    return element;
-}
